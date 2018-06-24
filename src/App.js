3337bb79be0abd8ae59417a0e9d319836904859b
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-06-24 11:30:36 +0300

    Make relative urls absolute to fix routing on Github pages.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import Map from './Map';
 const PATH_LIST = '/';
 const PATH_MAP = '/map';
 
+// Make relative url absolute to fix routing on Github pages.
+const getUrl = (path) => {
+  // Homepage set in package.json is exposed as process.env.PUBLIC_URL.
+  return process.env.PUBLIC_URL + path;
+};
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <div className="nav">
-            <Link to={PATH_LIST}>List</Link>
+            <Link to={getUrl(PATH_LIST)}>List</Link>
             {' | '}
-            <Link to={PATH_MAP}>Map</Link>
+            <Link to={getUrl(PATH_MAP)}>Map</Link>
           </div>
           <div>
-            <Route exact path={PATH_LIST} component={List}/>
-            <Route path={PATH_MAP} component={Map}/>
+            <Route exact path={getUrl(PATH_LIST)} component={List}/>
+            <Route path={getUrl(PATH_MAP)} component={Map}/>
           </div>
         </div>
       </Router>

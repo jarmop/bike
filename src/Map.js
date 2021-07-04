69364d7f007e9c7203640235da4282d76abc053d
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2021-07-04 12:02:03 +0300

    Config prettier to allow semicolons (by adding a config file to override global)

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -12,7 +12,10 @@ const CENTER_LATITUDE = 24.925;
 const CENTER_LONGITUDE = 60.168;
 
 const buildMap = (stations) => {
-  let map = L.map(CONTAINER_ID).setView([CENTER_LONGITUDE, CENTER_LATITUDE], 14);
+  let map = L.map(CONTAINER_ID).setView(
+    [CENTER_LONGITUDE, CENTER_LATITUDE],
+    14
+  );
 
   L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
@@ -24,13 +27,13 @@ const buildMap = (stations) => {
       zoomOffset: -1,
       id: 'mapbox/streets-v11',
     }
-  ).addTo(map)
+  ).addTo(map);
 
-  stations.forEach(station => {
+  stations.forEach((station) => {
     let classNames = ['bike-station'];
 
     if (!station.isOn) {
-      classNames.push('bike-station--off')
+      classNames.push('bike-station--off');
     } else if (station.bikesAvailable === 0) {
       classNames.push('bike-station--empty');
     } else if (station.bikesAvailable < 4) {
@@ -40,23 +43,19 @@ const buildMap = (stations) => {
     let divIcon = L.divIcon({
       html: station.bikesAvailable,
       className: classNames.join(' '),
-      iconSize: [BIKE_STATION_DIAMETER, BIKE_STATION_DIAMETER]
+      iconSize: [BIKE_STATION_DIAMETER, BIKE_STATION_DIAMETER],
     });
-    L.marker([station.lon, station.lat], {icon: divIcon}).addTo(map);
+    L.marker([station.lon, station.lat], { icon: divIcon }).addTo(map);
   });
-
-
 };
 
 class Map extends Component {
   componentDidMount() {
-    api.fetchStations().then(stations => buildMap(stations));
+    api.fetchStations().then((stations) => buildMap(stations));
   }
 
   render() {
-    return (
-        <div id="map" className="map"></div>
-    );
+    return <div id="map" className="map"></div>;
   }
 }
 

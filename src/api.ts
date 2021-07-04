a6808852296f3706378c0a6a47aaa43951957c27
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2021-07-04 16:27:07 +0300

    Add TypeScript

---
+++ src/api.ts
@@ -0,0 +1,31 @@
+const URL_DIGITRANSIT =
+  'https://api.digitransit.fi/routing/v1/routers/hsl/bike_rental';
+
+const STATION_STATE_ON = 'Station on';
+// const STATION_STATE_OFF = 'Station off';
+
+type Station = {
+  id: string;
+  name: string;
+  bikesAvailable: number;
+  lat: number;
+  lon: number;
+  isOn: boolean;
+};
+
+export const fetchStations = () => {
+  return fetch(URL_DIGITRANSIT)
+    .then((res) => res.json())
+    .then((json) => {
+      let stations: Array<Station> = json.stations.map((station: any) => ({
+        id: station.id,
+        name: station.name,
+        bikesAvailable: station.bikesAvailable,
+        lat: station.x,
+        lon: station.y,
+        isOn: station.state === STATION_STATE_ON,
+      }));
+
+      return stations;
+    });
+};

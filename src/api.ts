53f15500298f825f32d5f35c8d0cdb88cd5de07e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2021-07-04 16:47:15 +0300

    Exclude Vantaa stations

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
 const URL_DIGITRANSIT =
   'https://api.digitransit.fi/routing/v1/routers/hsl/bike_rental';
 
+const BIKE_NETWORK_ID = 'smoove';
+
 const STATION_STATE_ON = 'Station on';
 // const STATION_STATE_OFF = 'Station off';
 
@@ -13,18 +15,23 @@ type Station = {
   isOn: boolean;
 };
 
+const belongsToCorrectBikeNetwork = (stationData: any) =>
+  stationData.networks.includes(BIKE_NETWORK_ID);
+
 export const fetchStations = () => {
   return fetch(URL_DIGITRANSIT)
     .then((res) => res.json())
-    .then((json) => {
-      let stations: Array<Station> = json.stations.map((station: any) => ({
-        id: station.id,
-        name: station.name,
-        bikesAvailable: station.bikesAvailable,
-        lat: station.x,
-        lon: station.y,
-        isOn: station.state === STATION_STATE_ON,
-      }));
+    .then((data) => {
+      let stations: Array<Station> = data.stations
+        .filter(belongsToCorrectBikeNetwork)
+        .map((stationData: any) => ({
+          id: stationData.id,
+          name: stationData.name,
+          bikesAvailable: stationData.bikesAvailable,
+          lat: stationData.x,
+          lon: stationData.y,
+          isOn: stationData.state === STATION_STATE_ON,
+        }));
 
       return stations;
     });

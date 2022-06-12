db6f7ca0b1e7085f0809049a86710c6a809b6286
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2022-06-12 19:53:35 +0300

    Use default home location if location detection fails

--- src/Map.js
+++ src/Map.js
@@ -8,11 +8,24 @@ import * as api from './api';
 
 const CONTAINER_ID = 'map';
 const BIKE_STATION_DIAMETER = 18;
-const CENTER_LATITUDE = 24.925;
-const CENTER_LONGITUDE = 60.168;
+const HOME_LATITUDE = 60.240187;
+const HOME_LONGITUDE = 24.8722;
+const HOME_COORDINATES = [HOME_LATITUDE, HOME_LONGITUDE];
+const ZOOM = 14;
 
 const buildMap = (stations) => {
-  let map = L.map(CONTAINER_ID).locate({ setView: true, maxZoom: 14 });
+  let map = L.map(CONTAINER_ID);
+
+  map.on('locationerror', (errorEvent) => {
+    map.setView(HOME_COORDINATES, ZOOM);
+    console.error(errorEvent.message);
+  });
+
+  map.on('locationfound', (locationEvent) => {
+    map.setView(locationEvent.latlng, ZOOM);
+  });
+
+  map.locate();
 
   L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
@@ -20,7 +33,6 @@ const buildMap = (stations) => {
       attribution:
         '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
       tileSize: 512,
-      maxZoom: 18,
       zoomOffset: -1,
       id: 'mapbox/streets-v11',
     }
